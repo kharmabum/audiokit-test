de493c65acae7cb2faed0d7ff5206752afe99758
Juan-Carlos Foust <jc@other.xyz>
2017-01-17 18:44:07 -0800

    Initial Commit

---
+++ audiokit-testTests/audiokit_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  audiokit_testTests.swift
+//  audiokit-testTests
+//
+//  Created by Juan-Carlos Foust on 1/17/17.
+//  Copyright © 2017 Juan-Carlos Foust. All rights reserved.
+//
+
+import XCTest
+@testable import audiokit_test
+
+class audiokit_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
